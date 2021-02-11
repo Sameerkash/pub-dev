736de229ea7bc33757f58d774cd23c63cd4e4ba3
Istvan Soos <istvan.soos@gmail.com>
2021-02-11 15:43:23 +0100

    Better tool env with directory size reporting.

--- app/lib/shared/tool_env.dart
+++ app/lib/shared/tool_env.dart
@@ -8,6 +8,7 @@ import 'dart:io';
 import 'package:logging/logging.dart';
 import 'package:meta/meta.dart';
 import 'package:pana/pana.dart';
+import 'package:pool/pool.dart';
 
 import 'configuration.dart';
 
@@ -29,26 +30,39 @@ const _maxCount = 50;
 /// will be deleted and a new [_ToolEnvRef] with a new directory will be created.
 const _maxSize = 500 * 1024 * 1024; // 500 MB
 
+/// The id of the next [_ToolEnvRef] to be created.
+int _nextId = 0;
+
+/// The base temp directory for tool env.
+final _toolEnvTempDir = Directory.systemTemp.createTempSync('tool-env');
+
+/// The base directory for stable and preview SDKs.
+final _toolDir = Directory('/tool');
+
+/// Forcing callback processing into a single thread.
+final _pool = Pool(1);
+_ToolEnvRef _current;
+
 /// Calls [fn] with the [ToolEnvironment], handling the lifecycle of the local
 /// pub cache.
 Future<R> withToolEnv<R>({
   @required bool usesPreviewSdk,
   @required Future<R> Function(ToolEnvironment toolEnv) fn,
 }) async {
-  _ToolEnvRef ref;
-  try {
-    ref = await _getOrCreateToolEnvRef();
-    return await fn(usesPreviewSdk ? ref.preview : ref.stable);
-  } finally {
-    await ref?._release();
-  }
+  return await _pool.withResource(() async {
+    if (_current != null && !_current._isAvailable) {
+      await _current?._cleanup();
+      _current = null;
+    }
+    _current ??= await _createToolEnvRef();
+    _current._started++;
+    try {
+      return await fn(usesPreviewSdk ? _current.preview : _current.stable);
+    } finally {
+      await _current._checkSizeLimit();
+    }
+  });
 }
-
-/// The id of the next [_ToolEnvRef] to be created.
-int _nextId = 0;
-
-_ToolEnvRef _current;
-Completer _ongoing;
 
 /// Tracks the temporary directory of the downloaded package cache with the
 /// [ToolEnvironment] (that was initialized with that directory), along with its
@@ -63,91 +77,74 @@ class _ToolEnvRef {
   final ToolEnvironment preview;
   final _id = _nextId++;
   int _started = 0;
-  int _active = 0;
   bool _isAboveSizeLimit = false;
 
   _ToolEnvRef(this._pubCacheDir, this.stable, this.preview);
 
   bool get _isAvailable => _started < _maxCount && !_isAboveSizeLimit;
 
-  void _acquire() {
-    _started++;
-    _active++;
-    _logger
-        .info('($_id) Tool env acquired. started: $_started, active: $_active');
-  }
-
-  Future<void> _release() async {
-    _logger
-        .info('($_id) Tool env released. started: $_started, active: $_active');
-    await _checkSizeLimit();
-    _active--;
-    if (_active == 0) {
-      // Delete directory if the instance is no longer active or it reached the
-      // maximum threshold.
-      if (_started >= _maxCount || _current != this) {
-        _logger.info('($_id) Deleting pub cache dir: $_pubCacheDir');
-        await _pubCacheDir.delete(recursive: true);
-      }
-    }
+  Future<void> _cleanup() async {
+    _logger.info('($_id) Deleting pub cache dir: $_pubCacheDir');
+    await _pubCacheDir.delete(recursive: true);
   }
 
   Future<void> _checkSizeLimit() async {
     if (_isAboveSizeLimit) return;
-    int size = 0;
-    await for (var fse in _pubCacheDir.list(recursive: true)) {
-      if (fse is File) {
-        size += await fse.length();
-      }
-    }
+    final size = await _calcDirectorySize(_pubCacheDir);
     _logger.info('($_id) Current size of pub cache dir: $size');
     _isAboveSizeLimit = size > _maxSize;
   }
+
+  Future<void> _reportSizes() async {
+    int inMB(int size) => size ~/ (1024 * 1024);
+
+    final pubCacheSize = await _calcDirectorySize(_pubCacheDir);
+    final toolEnvTmpSize = await _calcDirectorySize(_toolEnvTempDir);
+    final systemTmpSize = await _calcDirectorySize(Directory.systemTemp);
+    final toolDirSize = await _calcDirectorySize(_toolDir);
+    _logger.info('($_id) Directory sizes: '
+        '${inMB(pubCacheSize)}MB pub cache, '
+        '${inMB(toolEnvTmpSize)}MB tool env temp, '
+        '${inMB(systemTmpSize)}MB system temp, '
+        '${inMB(toolDirSize)}MB tool SDK dir.');
+  }
 }
 
-/// Gets a currently available [_ToolEnvRef] if it is used less than the
-/// configured threshold ([_maxCount]). If it it has already
-/// reached the amount, a new cache dir and environment will be created.
-Future<_ToolEnvRef> _getOrCreateToolEnvRef() async {
-  _ToolEnvRef result;
-  while (result == null) {
-    if (_current != null && _current._isAvailable) {
-      result = _current;
-      result._acquire();
-      break;
+/// Creates a new [_ToolEnvRef] with a new pub cache dir.
+Future<_ToolEnvRef> _createToolEnvRef() async {
+  final cacheDir = await _toolEnvTempDir.createTemp('pub-cache-dir');
+  final resolvedDirName = await cacheDir.resolveSymbolicLinks();
+  final stableToolEnv = await ToolEnvironment.create(
+    dartSdkDir: envConfig.stableDartSdkDir,
+    flutterSdkDir: envConfig.stableFlutterSdkDir,
+    pubCacheDir: resolvedDirName,
+    environment: {
+      'FLUTTER_ROOT': envConfig.stableFlutterSdkDir,
+    },
+  );
+  final previewToolEnv = await ToolEnvironment.create(
+    dartSdkDir: envConfig.previewDartSdkDir,
+    flutterSdkDir: envConfig.previewFlutterSdkDir,
+    pubCacheDir: resolvedDirName,
+    environment: {
+      'FLUTTER_ROOT': envConfig.previewFlutterSdkDir,
+    },
+  );
+  final ref = _ToolEnvRef(cacheDir, stableToolEnv, previewToolEnv);
+  await ref._reportSizes();
+  return ref;
+}
+
+Future<int> _calcDirectorySize(Directory dir) async {
+  int size = 0;
+  await for (var fse in dir.list(recursive: true)) {
+    if (fse is File) {
+      try {
+        size += await fse.length();
+      } catch (_) {
+        // unable to read file size, permission missing
+      }
     }
-
-    if (_ongoing != null) {
-      await _ongoing.future;
-      continue;
-    }
-
-    _ongoing = Completer();
-
-    final cacheDir = await Directory.systemTemp.createTemp('pub-cache-dir');
-    final resolvedDirName = await cacheDir.resolveSymbolicLinks();
-    final stableToolEnv = await ToolEnvironment.create(
-      dartSdkDir: envConfig.stableDartSdkDir,
-      flutterSdkDir: envConfig.stableFlutterSdkDir,
-      pubCacheDir: resolvedDirName,
-      environment: {
-        'FLUTTER_ROOT': envConfig.stableFlutterSdkDir,
-      },
-    );
-    final previewToolEnv = await ToolEnvironment.create(
-      dartSdkDir: envConfig.previewDartSdkDir,
-      flutterSdkDir: envConfig.previewFlutterSdkDir,
-      pubCacheDir: resolvedDirName,
-      environment: {
-        'FLUTTER_ROOT': envConfig.previewFlutterSdkDir,
-      },
-    );
-    _current = _ToolEnvRef(cacheDir, stableToolEnv, previewToolEnv);
-    result = _current;
-    result._acquire();
-    _ongoing.complete();
-    _ongoing = null;
-    break;
   }
-  return result;
+  return size;
 }
