abc1552eb5abe5888bd2f6033a1ec6456f7a1fb7
Istvan Soos <istvan.soos@gmail.com>
2021-02-13 22:21:27 +0100

    no more git

--- app/script/setup-flutter.sh
+++ app/script/setup-flutter.sh
@@ -18,6 +18,9 @@ mkdir -p "$1"
 cd "$1"
 git clone -b "$2" --single-branch https://github.com/flutter/flutter.git flutter
 
+# Removing the .git folder turns off Flutter auto-upgrade checks (and ever-increasing git directory).
+rm -rf ./flutter/.git
+
 # Downloads the Dart SDK and disables analytics tracking – which we always want.
 # This will add 400 MB.
 ./flutter/bin/flutter config --no-analytics
